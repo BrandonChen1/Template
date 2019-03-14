b79107e46043426498c3b087878f3c557474244c
Brandon <bxc4081@rit.edu>
2019-03-14 14:34:38 -0400

    added package.json

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-var app = express();
 
-app
-    .use(express.static(path.join(__dirname, 'public')))
+var app = express();
+var io = require('socket.io')(app);
+
+app.use(express.static(path.join(__dirname, 'public')))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('index.ejs'))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
